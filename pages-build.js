ec79e86ef7b9d174218588ab138269413d0d887d
Archeb <archebasic@hotmail.com>
2024-07-23 11:41:40 +0800

    update page build script

--- pages-build.js
+++ pages-build.js
@@ -5,7 +5,7 @@ const path = require('path');
 var cloudCharaCardWorker = fs.readFileSync(path.resolve(__dirname, './cloud-characard-worker/src/index.js'), 'utf-8');
 
 // 进行编码
-cloudCharaCardWorker = "const worker_template=\"" + Buffer.from(cloudCharaCardWorker).toString('base64') + "\";"; 
+cloudCharaCardWorker = "\n\nconst worker_template=\"" + Buffer.from(cloudCharaCardWorker).toString('base64') + "\";"; 
 
 // 追加写入到 create-cloud-characard/js/character-card-processor.js.js
 fs.appendFileSync(path.resolve(__dirname, './create-cloud-characard/js/character-card-processor.js'), cloudCharaCardWorker, 'utf-8');
